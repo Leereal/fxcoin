83f4d23e1fee4060b895c0f47324c2c2fa7ab7c2
unknown <leereal08@ymail.com>
2020-09-06 17:31:20 +0200

    Seeder Payment Method

--- database/seeds/PaymentMethodSeeder.php
+++ database/seeds/PaymentMethodSeeder.php
@@ -11,6 +11,9 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // Truncate the table.
+        DB::table('payment_methods')->truncate();
         App\PaymentMethod::create(['name'=>'BITCOIN','avatar'=>'bitcoin.png','currency_id'=>'1']);
         App\PaymentMethod::create(['name'=>'SKRILL','avatar'=>'skrill.png','currency_id'=>'1']);
         App\PaymentMethod::create(['name'=>'NETELLER','avatar'=>'neteller.png','currency_id'=>'1']);
@@ -20,7 +23,7 @@ class PaymentMethodSeeder extends Seeder
         App\PaymentMethod::create(['name'=>'NEDBANK (SOUTH AFRICA)','avatar'=>'nedbank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'CAPITEC (SOUTH AFRICA)','avatar'=>'capitec.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'ABSA (SOUTH AFRICA)','avatar'=>'absa.png','currency_id'=>'2']);
-        App\PaymentMethod::create(['name'=>'AFRICAN BANK  (SOUTH AFRICA)','avatar'=>'african_bank.png','currency_id'=>'2']);
+        App\PaymentMethod::create(['name'=>'AFRICAN BANK  (SOUTH AFRICA)','avatar'=>'africa_bank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'BIDVEST (SOUTH AFRICA)','avatar'=>'bidvest.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'DISCOVERY (SOUTH AFRICA)','avatar'=>'discovery.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'GRINDROD (SOUTH AFRICA)','avatar'=>'grindrod.png','currency_id'=>'2']);
@@ -33,19 +36,20 @@ class PaymentMethodSeeder extends Seeder
         App\PaymentMethod::create(['name'=>'FIRST CAPITAL (BOTSWANA)','avatar'=>'first_capital.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'FNB (BOTSWANA)','avatar'=>'fnb.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'STANBIC (BOTSWANA)','avatar'=>'stanbic.png','currency_id'=>'2']);
-        App\PaymentMethod::create(['name'=>'STANDARD (BOTSWANA)','avatar'=>'standard.png','currency_id'=>'2']);
+        App\PaymentMethod::create(['name'=>'STANDARD (BOTSWANA)','avatar'=>'standard_bank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'NEDBANK (NAMIBIA)','avatar'=>'nedbank.png','currency_id'=>'2']);
-        App\PaymentMethod::create(['name'=>'STANDARD (NAMIBIA)','avatar'=>'standard.png','currency_id'=>'2']);
+        App\PaymentMethod::create(['name'=>'STANDARD (NAMIBIA)','avatar'=>'standard_bank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'TRUSTCO (NAMIBIA)','avatar'=>'trustco.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'ATLANTICO (NAMIBIA)','avatar'=>'atlantico.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'BANK BIC (NAMIBIA)','avatar'=>'bankbic.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'LETSHEGO (NAMIBIA)','avatar'=>'letshego.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'NEDBANK (SWAZILAND)','avatar'=>'nedbank.png','currency_id'=>'2']);
-        App\PaymentMethod::create(['name'=>'STANDARD (SWAZILAND)','avatar'=>'standard.png','currency_id'=>'2']);
+        App\PaymentMethod::create(['name'=>'STANDARD (SWAZILAND)','avatar'=>'standard_bank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'FNB (SWAZILAND)','avatar'=>'fnb.png','currency_id'=>'2']);
-        App\PaymentMethod::create(['name'=>'STANDARD (LESOTHO)','avatar'=>'standard.png','currency_id'=>'2']);
+        App\PaymentMethod::create(['name'=>'STANDARD (LESOTHO)','avatar'=>'standard_bank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'NEDBANK (LESOTHO)','avatar'=>'nedbank.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'FNB (LESOTHO)','avatar'=>'fnb.png','currency_id'=>'2']);
         App\PaymentMethod::create(['name'=>'MPESA (LESOTHO)','avatar'=>'mpesa.png','currency_id'=>'2']);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
