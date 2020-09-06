794646036febd10aac11c08075b8e91e6d28b75d
mufarofish <Mufaro.Hove@contractor.co.za>
2020-09-06 12:16:58 +0200

    mail template

---
+++ resources/views/mails/mail_register.blade.php
@@ -0,0 +1,21 @@
+Hello <i>{{ $pg_email->receiver }}</i>,
+<p>Welcome to Paygrid. FXAuction is an international peer to peer and pool crowdfunding platform. Bank investments take very long to mature and the Return On Investment is far way too low for the said period to meet the desired targets. FXAuction is established with the idea in mind of helping people get quicker returns on their investment so as to be able to meet their short-term financial goals! FXAuction is an online investment platform where you can offer to buy points for a selected period with a desired return.</p>
+<p>FXAuction you have the following packages to choose:</p>
+<ul>
+	<li>Pool Gold - This is a 7 day plan which offers a 30% return on investment</li>
+	<li>Pool Diamond - This is a 14 day plan which offers a 66% return on investment </li>
+	<li>Pool Platinum - This is a 30 day plan which offers a 190% return on investment </li>
+	<li>Peer to Peer (Gold) - 25% Profit in 7 Days</li>
+	<li>Peer to Peer (Diamond) - 60% Profit in 14 Days</li>
+	<li>Peer to Peer (Platinum) - 130% Profit in 30 Days</li>
+</ul>
+ 
+<div>
+<p>To login use these login details. &nbsp;<a href="http://login.paygrid.co.za">Click to login</a></p>
+<p><b>Username:</b>&nbsp;{{ $pg_email->username }}</p>
+<p><b>Password:</b>&nbsp;{{ $pg_email->password }}</p>
+</div>
+ 
+Thank You,
+<br/>
+<i><b>The FXAuction Team</b></i>
